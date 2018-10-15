e239db8d63f24773495fdb558c1bb372a1388e1e
Ajithesh <Ajithesh@DESKTOP-00FMAIN>
2018-10-15 13:28:33 +0530

    FirfoxDriver push

--- MagentoTest/src/test/java/MagentoTest.java
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -3,7 +3,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
@@ -13,7 +13,7 @@ public class MagentoTest {
 public void posCredentials( )throws Exception {
 	
 	String url="http://www.magento.com";
-	WebDriver driver=new ChromeDriver();
+	WebDriver driver=new FirefoxDriver();
 	
 	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	driver.get(url);
